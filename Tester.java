d97d716faad4f0c68026784bd4525c26b43f6d01
Ignacio J. Dotta <ij.dotta@gmail.com>
2020-09-15 15:34:53 -0300

    Cliente 2 modifica Tester.java

--- Tester.java
+++ Tester.java
@@ -2,7 +2,7 @@ public class Tester {
 	public static void main(String [] args) {
 		System.out.println("Hello world!");
 		
-		Alumno a = new Alumno("Ignacio", "Dotta", 126269);
+		Alumno a = new Alumno("Joaquín", "Dotta", 126269); //Cliente 2 modifica nombre Ignacio -> por -> Joaquín
 
 	}
 }
